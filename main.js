588c605a54b696e592bd7faa13eaa469e9d13091
Brandon Ashby <bash7325@gmail.com>
2022-03-12 11:18:22 -0700

    small updates to file structure

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
-
 const form = document.querySelector(".lorem-form");
 const numOfPara = document.getElementById("numofpara");
 const numOfParaRange = document.getElementById("numofparaRange");
 const result = document.querySelector(".lorem-text");
 
 function syncParaNumbers(e) {
-    const value = e.target.value;
-    numOfPara.value = value;
-    numOfParaRange.value = value;
-};
+  const value = e.target.value;
+  numOfPara.value = value;
+  numOfParaRange.value = value;
+}
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    const value = parseInt(numOfPara.value)
-    let tempText = text.slice(0, value);
-    tempText = tempText.sort(() => Math.random() - 0.5);
-    tempText = tempText.map(item => `<p class="result">${item}</p>`).join("");
-    result.innerHTML = tempText;
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const value = parseInt(numOfPara.value);
+  let tempText = text.slice(0, value);
+  tempText = tempText.sort(() => Math.random() - 0.5);
+  tempText = tempText.map((item) => `<p class="result">${item}</p>`).join("");
+  result.innerHTML = tempText;
 });
 
-numOfPara.addEventListener('input', syncParaNumbers);
-numOfParaRange.addEventListener('input', syncParaNumbers);
+numOfPara.addEventListener("input", syncParaNumbers);
+numOfParaRange.addEventListener("input", syncParaNumbers);
